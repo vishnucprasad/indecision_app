4f0545dfa9c9dffea9e281b0149a0f7debca1df6
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2021-02-11 07:44:06 +0530

    Conditional Rendering in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,8 @@
 
 var app = {
     title: 'Indecision App',
-    subTitle: 'Put your life in the hands of a computer.'
+    subTitle: 'Put your life in the hands of a computer.',
+    options: ['One', 'Two']
 };
 
 var template = React.createElement(
@@ -13,11 +14,12 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subTitle && React.createElement(
         'p',
         null,
         app.subTitle
     ),
+    app.options.length > 0 ? 'Here are your options' : 'No options',
     React.createElement(
         'ol',
         null,
@@ -40,26 +42,32 @@ var user = {
     location: 'Kanjirappally'
 };
 
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
 var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        user.name.toUpperCase()
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 var app = {
     title: 'Indecision App',
-    subTitle: 'Put your life in the hands of a computer.'
+    subTitle: 'Put your life in the hands of a computer.',
+    options: ['One', 'Two']
 };
 
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subTitle}</p>
+        {app.subTitle && <p>{app.subTitle}</p>}
+        {app.options.length > 0 ? 'Here are your options' : 'No options'}
         <ol>
             <li>Item One</li>
             <li>Item Two</li>
@@ -20,11 +22,17 @@ var user = {
     location: 'Kanjirappally'
 };
 
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
 var templateTwo = (
     <div>
-        <h1>{user.name.toUpperCase()}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
