4702d91bd5e4c653305d16879d8df817924fa314
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2021-02-23 09:49:35 +0530

    Method Binding

--- src/app.js
+++ src/app.js
@@ -40,8 +40,13 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
   handleRemoveAll() {
-    alert("Handle Remove All");
+    console.log(this.props.options);
+    // alert("Handle Remove All");
   }
   render() {
     return (
