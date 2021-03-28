ad25b81e88fe1ce535f23066e10ef0edae119baf
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2021-03-28 11:49:55 +0530

    Styling the Options List

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import Option from './Option';
+import React from "react";
+import Option from "./Option";
 
 const Options = (props) => (
-    <div>
-        <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an option to get started.</p>}
-        {props.options.map((option) => (
-            <Option
-                key={option}
-                optionText={option}
-                handleDeleteOption={props.handleDeleteOption}
-            />
-        ))}
+  <div>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+      </button>
     </div>
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started.</p>}
+    {props.options.map((option) => (
+      <Option
+        key={option}
+        optionText={option}
+        handleDeleteOption={props.handleDeleteOption}
+      />
+    ))}
+  </div>
 );
 
 export default Options;
